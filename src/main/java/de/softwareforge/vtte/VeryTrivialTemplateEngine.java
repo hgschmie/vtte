95897743249961d7c6c38284c49836a52969628c
Henning Schmiedehausen <henning@schmiedehausen.org>
2014-02-24 17:53:50 -0800

    update base pom, add pmd rules

--- src/main/java/de/softwareforge/vtte/VeryTrivialTemplateEngine.java
+++ src/main/java/de/softwareforge/vtte/VeryTrivialTemplateEngine.java
@@ -1,6 +1,19 @@
 /*
- * Copyright (C) 2013 Henning Schmiedehausen
+ * Copyright (C) 2013-2014 Henning Schmiedehausen
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -47,7 +60,8 @@ public class VeryTrivialTemplateEngine
         final Renderer r = new Renderer(0, rootTemplate, unmodifiableMap(properties));
         final StringBuilder sb = new StringBuilder();
         r.render(sb);
-        return sb.toString();
+        final String result = sb.toString();
+        return result;
     }
 
     private final class Renderer
